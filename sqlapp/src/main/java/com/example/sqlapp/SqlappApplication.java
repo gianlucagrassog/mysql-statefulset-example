75cad5abe3f22ff6d088e47dc0631065ff047ec9
gianlucagrassog <gianluca.grasso8@studium.unict.it>
2022-12-18 10:37:20 +0100

    Initial commit

---
+++ sqlapp/src/main/java/com/example/sqlapp/SqlappApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlappApplication.class, args);
+    }
+
+}
---
+++ sqlapp/src/test/java/com/example/sqlapp/SqlappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqlapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
